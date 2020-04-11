dd0d79552567dad947bea02a748352c4a76dcb39
SASWAT123 <psaswat21@gmail.com>
2020-04-11 23:39:27 +0530

    Updates

---
+++ precache-manifest.21405c4eb6b1d7d57c8e0e17b51693e1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be160bfb182f1c8b5ecc5567351b9a99",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "95ff33e41aae9524dbc0",
+    "url": "/portfolio/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "3ccaa865d4bcf1686f4e",
+    "url": "/portfolio/static/css/main.74416d6f.chunk.css"
+  },
+  {
+    "revision": "95ff33e41aae9524dbc0",
+    "url": "/portfolio/static/js/2.824a4dab.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.824a4dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ccaa865d4bcf1686f4e",
+    "url": "/portfolio/static/js/main.ab3a1127.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "9783697c529b69afe036740c87460d05",
+    "url": "/portfolio/static/media/Saswat.9783697c.jpg"
+  },
+  {
+    "revision": "2c2c1efab929785d95ba7ce9994ab236",
+    "url": "/portfolio/static/media/card_img.2c2c1efa.jpg"
+  }
+]);
